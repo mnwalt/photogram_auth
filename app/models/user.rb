c2ec715384b7c0e703ed4f20286e3e6f8f1ae474
mnwalt <mnwalt@users.noreply.github.com>
2016-11-18 13:33:41 -0600

    session 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
 
   validates :username, :presence => true, :uniqueness => true
   validates :email, :presence => true
-  validates :password, :presence => true
+  validates :password, :presence => true, on: :create
+  validates :password, :presence => true, on: :update, allow_blank: true
+
 
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
